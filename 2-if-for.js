429ea2920efc7b74f27f49b23402d106afbe27e6
Sansinee <gift.chill.chill@gmail.com>
2019-09-21 12:21:15 +0700

    add const,let,var,if,for,function,class

---
+++ 2-if-for.js
@@ -0,0 +1,36 @@
+// const a = "a";
+// if (a === "a") {
+//   console.log("It's a");
+// } else {
+//   console.log("It's not a");
+// }
+
+// for (var i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+// console.log(i);
+
+// let sum =0;
+// for (let i =1;i<=1000;i++){
+//     sum +=i;
+// }
+// console.log(i);
+
+let a = 1;
+let b = 1.1;
+let c = "string";
+let d = true;
+let e = false;
+let f = ["a", "b", "c"];
+f[0] = "f";
+f.push("h");
+console.log(f);
+let h = [];
+let studen = {
+  id: "620001",
+  name: "Sansinee",
+  faculty: "swe"
+};
+console.log(studen.name);
+studen.name = "Sansinee";
+console.log(studen.name);
